cf8d0b24f3807790f6be6d94331312f2fab7ddaa
Pablo Izquierdo <pabloizquierdo2395@gmail.com>
2022-05-20 13:23:09 -0400

    Initialize project using Create React App

---
+++ GifExpertApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function GifExpertApp(props) {
+  return (
+    <>
+      <h2></h2>
+      <hr />
+    </>
+  );
+}
+
+GifExpertApp.propTypes = {};
+
+export default GifExpertApp;
